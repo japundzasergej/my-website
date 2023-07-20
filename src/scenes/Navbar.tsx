95a652fd279830b0bc49f295b84588552e1fa6fc
SERGEJ_JAPUNDZA <japundzasergej@gmail.com>
2023-07-20 17:25:09 +0200

    mobile improvements

--- src/scenes/Navbar.tsx
+++ src/scenes/Navbar.tsx
@@ -2,11 +2,13 @@ import Link from '../components/Link';
 
 import lightLogo from '../assets/light.ico';
 import darkLogo from '../assets/dark.ico';
+import mLightLogo from '../assets/m-light.png';
+import mDarkLogo from '../assets/m-dark.png';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import nightMode from '../assets/night-mode.png';
 import lNightMode from '../assets/l-night-mode.png';
-import mNightMode from '../assets/m-night-mode.png'
-import mLightMode from '../assets/m-l-night-mode.png'
+import mNightMode from '../assets/m-night-mode.png';
+import mLightMode from '../assets/m-l-night-mode.png';
 
 import { motion } from 'framer-motion';
 
@@ -14,16 +16,11 @@ import { RxHamburgerMenu } from 'react-icons/rx';
 import { AiOutlineClose } from 'react-icons/ai';
 
 import { useAppDispatch, useAppSelector } from '../hooks/useTypedHooks';
-import {
-  setIsMenuOpen,
-  setIsDark,
-} from '../features/portfolioSlice';
+import { setIsMenuOpen, setIsDark } from '../features/portfolioSlice';
 import useMediaQuery from '../hooks/useMediaQuery';
 
-
 type Links = { id: number; link: string };
-type Links2 = { id: number; link: string, title:string };
-
+type Links2 = { id: number; link: string; title: string };
 
 const Navbar = () => {
   const links: Links[] = [
@@ -37,8 +34,8 @@ const Navbar = () => {
     { id: 1, link: 'm-home', title: 'home' },
     { id: 2, link: 'm-about', title: 'about' },
     { id: 3, link: 'm-portfolio', title: 'portfolio' },
-    { id: 4, link: 'm-experience', title: 'experience'  },
-    { id: 5, link: 'm-contact', title: 'contact'  },
+    { id: 4, link: 'm-experience', title: 'experience' },
+    { id: 5, link: 'm-contact', title: 'contact' },
   ];
   const container = {
     hidden: { opacity: 0 },
@@ -75,7 +72,15 @@ const Navbar = () => {
     >
       <article className="flex justify-between h-full w-full">
         <div className=" flex justify-center items-center mx-auto">
-          <img src={isDark ? darkLogo : lightLogo} alt="logo"/>
+          <img
+            src={
+              isDark
+                ? `${isDesktop ? darkLogo : mDarkLogo}`
+                : `${isDesktop ? lightLogo : mLightLogo}`
+            }
+            alt="logo"
+            className='newScreen:scale-100 scale-50'
+          />
         </div>
         {isDesktop ? (
           <div className="flex widescreen:gap-12 gap-8 md:mx-auto py-6 lg:text-lg text-sm font-noto">
@@ -104,10 +109,10 @@ const Navbar = () => {
               <button
                 className={`${
                   isDark ? 'm-toggle' : 'm-off '
-                } relative before:absolute before:content-start before:-right-20 before:top-0 `}
+                } relative before:absolute before:content-start before:-left-20 before:top-0 `}
                 onClick={() => dispatch(setIsDark())}
               >
-                <img src={isDark ? mLightMode : mNightMode} alt="night mode"  />
+                <img src={isDark ? mLightMode : mNightMode} alt="night mode" />
               </button>
             </div>
             <motion.div
@@ -144,20 +149,32 @@ const Navbar = () => {
                 className={`${isDark ? 'text-white' : 'text-navy-blue'}`}
               />
             </div>
-            
-            <motion.div variants={container} initial="hidden" animate="show" className='z-40'>
+
+            <motion.div
+              variants={container}
+              initial="hidden"
+              animate="show"
+              className="z-40"
+            >
               {links2.map(({ id, link, title }) => (
                 <motion.div
                   key={id}
-                  className={`text-center relative md:text-7xl sm:text-6xl text-4xl text-black py-5 font-black ${link} before:absolute md:before:-left-28 before:-left-20 before:top-10 py-8 ${isDark ? 'text-white': 'text-navy-blue'} border-b-2 flex justify-center border-sky-blue font-noto cursor-pointer`}
+                  className={`text-center relative md:text-7xl sm:text-6xl text-4xl text-black py-5 font-black ${link} before:absolute md:before:-left-28 before:-left-20 before:top-10 py-8 ${
+                    isDark ? 'text-white' : 'text-navy-blue'
+                  } border-b-2 flex justify-center border-sky-blue font-noto cursor-pointer`}
                   variants={item}
                   whileHover={{ scale: 1.1 }}
                 >
-                  <AnchorLink href={`#${title}`} onClick={() => dispatch(setIsMenuOpen())}>{title.toUpperCase()}</AnchorLink>
+                  <AnchorLink
+                    href={`#${title}`}
+                    onClick={() => dispatch(setIsMenuOpen())}
+                  >
+                    {title.toUpperCase()}
+                  </AnchorLink>
                 </motion.div>
               ))}
             </motion.div>
-            <div className='bg-abstract absolute h-full w-full bg-cover z-0'></div>
+            <div className="bg-abstract absolute h-full w-full bg-cover z-0"></div>
           </motion.div>
         )}
       </article>
