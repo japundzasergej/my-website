132f8ac7c1be9a2ff16c8146251e8f0c4c31e783
SERGEJ_JAPUNDZA <japundzasergej@gmail.com>
2023-07-20 17:48:06 +0200

    legal

--- src/scenes/Footer.tsx
+++ src/scenes/Footer.tsx
@@ -3,8 +3,8 @@ import SocialMedia from '../components/SocialMedia';
 const Footer = () => {
   return (
     <section className="w-full min-h-20 bg-light-beige">
-      <article className="flex justify-between items-center text-sm px-2 py-4">
-        <div className="md:mx-auto flex flex-col md:text-base text-xs px-4">
+      <article className="flex justify-between items-center px-2 py-4">
+        <div className="md:mx-auto flex flex-col md:text-sm text-xs px-4">
           <span>
             <a
               href="https://www.flaticon.com/free-icons/web-development"
@@ -28,7 +28,28 @@ const Footer = () => {
           </span>
           <span>
             <a href="https://pngtree.com/freepng/abstract-modern-blue-purple-wave-background_5508041.html?sol=downref&id=bef">
-              backdrop PNG Designed By 4ziz from
+              backdrop PNG Designed By 4ziz
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.flaticon.com/free-icons/toggle-button"
+              title="toggle button icons"
+            >
+              Toggle button icons created by FR_Media - Flaticon
+            </a>
+          </span>
+          <span>
+            <a
+              href="https://www.flaticon.com/free-icons/toggle-button"
+              title="toggle button icons"
+            >
+              Toggle button icons created by FR_Media - Flaticon
+            </a>
+          </span>
+          <span>
+            <a href="https://pngtree.com/freepng/geometric-shapes-sets-with-multi-color-gradients_3652063.html?sol=downref&id=bef">
+              shapes clipart PNG Designed By akshaydhameliya
             </a>
           </span>
         </div>
